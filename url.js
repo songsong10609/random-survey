93479f64e2c0715f231fc9d625166f5c60bd2b06
songsong10609 <songsong10609@gmail.com>
2026-07-03 12:58:03 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://forms.gle/CNoTZNyj8rWakRZz8',
+	'https://forms.gle/gKNGssfhGAEVSvB98',
 ];
